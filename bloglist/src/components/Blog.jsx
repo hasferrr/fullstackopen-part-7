45ad7a6270798335e0808926e511a0ec327211bb
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-02-12 17:46:58 +0700

    finish 7.18 - implements comments view

--- bloglist/src/components/Blog.jsx
+++ bloglist/src/components/Blog.jsx
@@ -40,6 +40,15 @@ const Blog = ({ blog, updateBlog, deleteBlog }) => {
           <></>
         )}
       </div>
+
+      <h3>comments</h3>
+      <ul>
+        {blog.comments
+          ? blog.comments.map((comment, index) => (
+              <li key={index}>{comment}</li>
+            ))
+          : null}
+      </ul>
     </div>
   )
 }
